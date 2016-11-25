f195668a65a384b7ee86e26d0fdb007db60bc5f3
Croicu Catalin <castan2003@gmail.com>
2016-11-25 15:15:23 +0200

    Am adaugat un controler, un view si model(Movie); Am adaugat un nu stil de afisare;

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;//adaug referinta manual
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Sherek!" ,Id=1234};
+            return View(movie);  
+        }
+    }
+}
